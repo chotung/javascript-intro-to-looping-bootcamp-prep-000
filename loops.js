866b0ab5eee384e6b2eb8099f0d04ae1fc6572f1
Choeles Tung <choelestung@gmail.com>
2018-05-08 02:27:03 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,3 +19,13 @@ function whileLoop(number){
 }
 
 
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+  return array;
+}
